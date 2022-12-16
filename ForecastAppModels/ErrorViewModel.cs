f80cf52f72dd4c58caaccbc81a819777a24a4dbe
ivelinamisova <ivelina_misova@abv.bg>
2022-12-16 21:51:19 +0200

    first commit

---
+++ ForecastAppModels/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MicroserviceWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
